adc5a36132a1cff5960c96c296ccbbd0085ae33f
bobabagus <121292474+bobabagus@users.noreply.github.com>
2022-12-30 19:34:33 +0700

    asd

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,14 @@ const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+// bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
 bot.command('about', about());
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+// bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+// process.once('SIGINT', () => bot.stop('SIGINT'));
+// process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {

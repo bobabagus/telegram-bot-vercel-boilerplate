eec443d44c079555a189877d6caca84f05a4480a
bobabagus <121292474+bobabagus@users.noreply.github.com>
2022-12-30 19:42:57 +0700

    hi

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
 bot.command('about', about());
-// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 // bot.launch();
 
 // Enable graceful stop

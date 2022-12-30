fa5d6adffca6cdcbcb4a4c51e295f3c41132b6a5
bobabagus <121292474+bobabagus@users.noreply.github.com>
2022-12-30 14:10:04 +0700

    test7

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -13,7 +13,7 @@ const greeting = () => async (ctx: Context) => {
 
   const messageId = ctx.message?.message_id;
   const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
-  const temp1 = ctx.message;
+  const temp1 = JSON.stringify(ctx.message.text);
 
   if (messageId) {
     await replyToMessage(ctx, messageId, temp1);

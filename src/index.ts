03d8130b2098969b5a3e4f7273949afb1e29f796
bobabagus <121292474+bobabagus@users.noreply.github.com>
2022-12-30 19:47:26 +0700

    state role

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,12 @@ const bot = new Telegraf(BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.on(message('text'), async (ctx) => {
+  // Using context shortcut
+  await ctx.reply(`Hello ${ctx.state.role}`);
+});
 bot.command('about', about());
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-// bot.launch();
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));

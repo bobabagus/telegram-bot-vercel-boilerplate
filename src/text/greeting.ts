8236b76590a4e16bdd9bcc5fe55993291913c3fe
bobabagus <121292474+bobabagus@users.noreply.github.com>
2022-12-30 15:56:37 +0700

    123123

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -16,8 +16,29 @@ const greeting = () => async (ctx: Context) => {
   const objs = JSON.stringify(ctx.message);
   const objs2 = JSON.parse(objs)
   const msgs = objs2.text
+  const password = 'Bobabagus'
+  let arr = msgs.split('\n')
+  let postObjects: any = {}
+  arr.forEach((ar: any, ari: any) => {
+    let keyval: any = ar.split(': ')
+    if (keyval[1]) {
+      postObjects[keyval[0]] = keyval[1]
+    }
+  })
+  let thepost: any = `[${postObjects?.name}](${postObjects?.link})
+Type : ${postObjects?.type.toUpperCase()}
+Size : ${postObjects?.size.toUpperCase()}
+Content: ${postObjects?.vid} Videos, ${postObjects?.img} Images
+Password: ${password}
+
+>>> Link Download [${postObjects.name}](${postObjects.link})
+
+Buat yg Error Silahkan Pakai Cloudflare, APP nya bisa download di : https://1.1.1.1/
+
+Visit New Web : https://openboba.shop
+Link Rusak atau mati silahkan comment`
   if (messageId) {
-    await replyToMessage(ctx, messageId, `hello ${userName} ${msgs}`);
+    await replyToMessage(ctx, messageId, `hello ${userName} ${thepost}`);
   }
 };
 
